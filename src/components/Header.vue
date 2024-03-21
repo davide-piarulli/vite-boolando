b6dcbaf1687f515ebb1854d588493641f38e2d0a
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-21 17:43:40 +0000

    [add] header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,7 @@
 <script>
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
 };
 </script>
@@ -21,22 +19,67 @@ export default {
         </nav>
 
         <div class="logo">
-          <img src="../assets/img/boolean-logo.png" alt="Booleando Logo">
+          <img src="../assets/img/boolean-logo.png" alt="Booleando Logo" />
         </div>
 
         <div class="menu right">
           <nav>
             <ul class="d-flex">
-              <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
-              <li><a href="#"><i class="fa-regular fa-heart"></i></a></li>
-              <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+              <li>
+                <a href="#"><i class="fa-solid fa-user"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-regular fa-heart"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+              </li>
             </ul>
           </nav>
         </div>
-
       </div>
     </div>
   </header>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables" as *;
+header {
+  background-color: $bg-color-header;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  width: 100%;
+  box-shadow: 0 1px 3px grey;
+  .topbar {
+    padding: 10px;
+    img {
+      max-width: 150px;
+    }
+    .container {
+      justify-content: space-between;
+      align-items: center;
+    }
+    .menu {
+      width: 20%;
+      ul a {
+        color: white;
+        text-decoration: none;
+        font-size: 20px;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+    .left li {
+      padding-right: 10px;
+    }
+    .right li {
+      padding-right: 10px;
+    }
+    .right ul {
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import '@fortawesome/fontawesome-free/css/all.css' // Importa i file di stile di Font Awesome
+import '@fortawesome/fontawesome-free/js/all.js' // Importa il file JavaScript di Font Awesome
 createApp(App).mount('#app')
