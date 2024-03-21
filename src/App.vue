19935693629cedfb36701cf41da0d39dcdd6fc15
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-21 17:01:18 +0000

    [add] import header raw

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@ import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 import Footer from './components/Footer.vue';
 export default {
-  data() {
-    return {
+  components: {
       Header,
       Main,
       Footer,
-    };
+  
   },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,32 @@ export default {
 
 <template>
   <header>
-    header
+    <div class="topbar">
+      <div class="container d-flex">
+        <nav class="menu left">
+          <ul class="d-flex">
+            <li><a href="#">Donna</a></li>
+            <li><a href="#">Uomo</a></li>
+            <li><a href="#">Bambini</a></li>
+          </ul>
+        </nav>
+
+        <div class="logo">
+          <img src="../assets/img/boolean-logo.png" alt="Booleando Logo">
+        </div>
+
+        <div class="menu right">
+          <nav>
+            <ul class="d-flex">
+              <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+              <li><a href="#"><i class="fa-regular fa-heart"></i></a></li>
+              <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+    </div>
   </header>
 </template>
 
