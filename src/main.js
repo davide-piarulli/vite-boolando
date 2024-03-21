0ddc9c1b98c3474f355e3637f5241ee16ee59dfc
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-21 17:48:31 +0000

    [add] font

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import './style.css'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css' // Importa i file di stile di Font Awesome
 import '@fortawesome/fontawesome-free/js/all.js' // Importa il file JavaScript di Font Awesome
+import '@fontsource/roboto';
 createApp(App).mount('#app')
