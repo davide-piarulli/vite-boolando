8656921345571e63632d9ff29d84544124257a47
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-24 17:30:14 +0000

    [add] first image

--- src/components/partials/ProductCard.vue
+++ src/components/partials/ProductCard.vue
@@ -1,48 +1,44 @@
 <script>
-  export default {
-    props: {
-      product: Object,
-
+export default {
+  props: {
+    product: Object,
+  },
+  methods: {
+    getImagePath(img) {
+      return new URL(`../../assets/img/${img}`, import.meta.url).href;
     },
-    mounted(){
-
-    }
-  }
+  },
+};
 </script>
 
 <template>
   <main>
-    
-        <a href="#" class="card">
-          <!-- <img src="../../assets/img/1.webp" alt="image1"> -->
-          <img :src="product.frontImage" alt="image1">
-          <img src="" alt="" class="sec-image">
-          <span class="heart">&hearts;</span>
-          <div class="badges">
-            <span class="badge disc">-50%</span>
-            <span class="badge sost">Sostenibilità</span>
-          </div>
-          <div class="info">
-            <span class="brand">{{product.brand}}</span>
-            <h4 class="prod-title">{{product.name}}</h4>
-            <span class="prod-disc-price">14.99 €</span>
-            <span class="prod-price">{{product.price}} €</span>
-          </div>
-        </a>
-
+    <a href="#" class="card">
+      <!-- <img src="../../assets/img/1.webp" alt="image1"> -->
+      <img :src="getImagePath(product.frontImage)" :alt="product.frontImage" />
+      <img src="" alt="" class="sec-image" />
+      <span class="heart">&hearts;</span>
+      <div class="badges">
+        <span class="badge disc">-50%</span>
+        <span class="badge sost">Sostenibilità</span>
+      </div>
+      <div class="info">
+        <span class="brand">{{ product.brand }}</span>
+        <h4 class="prod-title">{{ product.name }}</h4>
+        <span class="prod-disc-price">14.99 €</span>
+        <span class="prod-price">{{ product.price }} €</span>
+      </div>
+    </a>
   </main>
 </template>
-
-
 
 <style lang="scss" scoped>
 @use "../../assets/scss/partials/variables" as *;
 .card {
   margin-bottom: 20px;
   position: relative;
-  &:hover{
-  opacity: 0.6;
-
+  &:hover {
+    opacity: 0.6;
   }
   img {
     max-width: 320px;
@@ -63,37 +59,37 @@
       color: red;
     }
     .badges {
-    position: absolute;
-    bottom: 80px;
-    left: 0;
-  }
-.badge{
-    padding: 5px 10px;
-    color: white;
-    font-weight: 700;
-  }
-  .disc {
-    background-color: red;
-  }
-  .sost {
-    background-color: #008000;
-  }
-  .brand {
-    font-size: 0.8rem;
-  }
-  .prod-disc-price {
-    color: #ff0000;
-    font-weight: 800;
-  }
-  .prod-price {
-    text-decoration: line-through;
-  }
-  .sec-image {
-    display: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+      position: absolute;
+      bottom: 80px;
+      left: 0;
+    }
+    .badge {
+      padding: 5px 10px;
+      color: white;
+      font-weight: 700;
+    }
+    .disc {
+      background-color: red;
+    }
+    .sost {
+      background-color: #008000;
+    }
+    .brand {
+      font-size: 0.8rem;
+    }
+    .prod-disc-price {
+      color: #ff0000;
+      font-weight: 800;
+    }
+    .prod-price {
+      text-decoration: line-through;
+    }
+    .sec-image {
+      display: none;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
   }
 }
 .card:hover .sec-image {
